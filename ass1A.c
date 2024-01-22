a7a852eab03a44e819514048ebb7c94bccb0148e
snxerame <128610961+snxerame@users.noreply.github.com>
2024-01-22 22:48:27 +0530

    Update ass1A.c

--- ass1A.c
+++ ass1A.c
@@ -1,58 +1,34 @@
 #include <stdio.h>
-
 int main() {
-    char a[100];
-    char b[100];
+    char a[100]; char b[100];
     int r[1000] = {0};
     int dr[1000] = {0};
-
-    printf("Enter the First Number:    ");
-    scanf("%s", a);
-    getchar();
-
-    printf("Enter the Second Number:    ");
-    scanf("%s", b);
-    getchar();
-
+    scanf("%s", a);scanf("%s", b);
     int lenA, lenB, lenr;
     int carry = 0;
-
     for (lenA = 0; a[lenA] != '\0'; lenA++) {}
     for (lenB = 0; b[lenB] != '\0'; lenB++) {}
-
     for (int i = 0; i < lenA; i++) {
         int digitA = a[lenA - i - 1] - '0';
         carry = 0;
-
         for (int j = 0; j < lenB; j++) {
             int digitB = b[lenB - j - 1] - '0';
             int product = digitA * digitB + r[i + j] + carry;
             carry = product / 10;
-            r[i + j] = product % 10;
-        }
-
-        r[i + lenB] += carry;
-    }
-
+            r[i + j] = product % 10;}
+        r[i + lenB] += carry;}
     lenr = lenA + lenB;
-
     int carrysum = 0;
     for (int k = 0; k < lenr; k++) {
         int digitsum = dr[k] + r[k] + carrysum;
         dr[k] = digitsum % 10;
-        carrysum = digitsum / 10;
-    }
-
+        carrysum = digitsum / 10;}
     while (carrysum) {
         dr[lenr] = carrysum % 10;
         carrysum /= 10;
-        lenr++;
-    }
-
-    // Print the result in reverse order
-    for (int n = lenr - 1; n >= 0; n--) {
-        printf("%d", dr[n]);
-    }
-
-    return 0;
-}
+        lenr++;}
+    int m;
+    if(dr[lenr-1]!=0) m = lenr -1;
+    else m = lenr - 2;
+    for (int n = m; n >= 0; n--)
+        printf("%d", dr[n]);}
